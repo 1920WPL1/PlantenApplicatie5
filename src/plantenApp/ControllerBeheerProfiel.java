c5805fd9d931cad372efe530c4d0bc0f96e59a37
Matthias Vancoillie <matthias.vancoillie@student.vives.be>
2020-06-09 12:14:04 +0200

    Aanmaken BeheerProfiel en hermaken schermen wachtwoorden

---
+++ src/plantenApp/ControllerBeheerProfiel.java
@@ -0,0 +1,22 @@
+package plantenApp;
+
+import javafx.scene.input.MouseEvent;
+
+import java.awt.*;
+
+public class ControllerBeheerProfiel {
+    public Label lblVoornaamProfiel;
+    public Label lblAchternaamProfiel;
+    public Label lblEmailProfiel;
+
+    public Button btnWachtwoordWijzigen;
+    public Button btnTerugHoofdscherm;
+
+
+
+    public void clicked_WWWijzigenProfiel(MouseEvent mouseEvent) {
+    }
+
+    public void clicked_TerugHoofdscherm(MouseEvent mouseEvent) {
+    }
+}
