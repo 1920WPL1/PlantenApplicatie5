1216e31dc6a1e1004dafe931da3a524c7dbc4768
Matthias Vancoillie <matthias.vancoillie@student.vives.be>
2020-06-09 14:01:09 +0200

    Start beheerprofiel controller

--- src/plantenApp/ControllerBeheerProfiel.java
+++ src/plantenApp/ControllerBeheerProfiel.java
@@ -1,8 +1,10 @@
 package plantenApp;
 
 import javafx.scene.input.MouseEvent;
+import plantenApp.java.model.Gebruiker;
 
 import java.awt.*;
+import java.sql.Connection;
 
 public class ControllerBeheerProfiel {
     public Label lblVoornaamProfiel;
@@ -12,11 +14,19 @@ public class ControllerBeheerProfiel {
     public Button btnWachtwoordWijzigen;
     public Button btnTerugHoofdscherm;
 
+    private Connection connection;
+    private Gebruiker gebruiker;
+
+
+
 
 
     public void clicked_WWWijzigenProfiel(MouseEvent mouseEvent) {
+        // het wijzigen van een wachtwoord van je profiel.
     }
 
     public void clicked_TerugHoofdscherm(MouseEvent mouseEvent) {
+        // het terugkeren naar het hoofdscherm
+
     }
 }
