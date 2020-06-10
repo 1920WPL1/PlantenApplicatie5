b79ba22a550f3ac8b705f41d6fd4665125ff5cab
Jasper De Cooman <jasperdecooman@gmail.com>
2020-06-10 10:52:28 +0200

    Wachtwoord wijzigen: knop "Terug" werkt

--- src/plantenApp/ControllerWachtWoordVergeten.java
+++ src/plantenApp/ControllerWachtWoordVergeten.java
@@ -3,13 +3,21 @@ package plantenApp;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import plantenApp.java.model.LoginMethods;
 
 
 public class ControllerWachtWoordVergeten {
     public TextField txtEmail;
     public Button btnVerstuurEmail;
+    public Button btnTerug;
+    public AnchorPane anchorPane;
 
     public void clicked_MailVersturen(MouseEvent mouseEvent) {
 
+    }
+
+    public void clicked_Terug(MouseEvent mouseEvent) {
+        LoginMethods.loadScreen(anchorPane, getClass(), "view/HoofdScherm.fxml");
     }
 }
